8a23951780faaa88725d9c83e14247875a4b0e19
Stefan <feynman@localhost.localdomain>
2015-09-09 16:36:13 +0200

    lowPass?

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7/80e1a033e15600151850bcd9576d41d6
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "sensor.h"
+
+// Filter function declerations
+int lowPass(int x[], int y[]);
+int highPass(int x[], int y[]);
+int derivative(int x[], int y[]);
+int squaring(int y[]);
+int movingWindowIntegration(int x[], int y[]);
+
+// Main program
+int main(int argc, char *argv[]) {
+	int x[32], y[32];
+
+	printf("%i\n", getNextData());
+	printf("%i\n", getNextData());
+
+
+	return 0;
+}
+
+
+// Filters here
+int lowPass(int x[], int y[]) {
+	int n = 32;
+	y[n] = 2 * y[n-1] - y[n-2] + 1 / 32 * (x[n] - 2 * x[n-6] + x[n-12]);
+	return y[n];
+}
+
+int highPass(int x[], int y[]) {
+	int n = 32;
+	y[n] = y[n-1] - x[n] / 32 + x[n-16] - x[n-17] + x[n-32] / 32;
+	return y[n];
+}
+
+int derivative(int x[], int y[]) {
+	int n = 32;
+	y[n] = 1 / 8 * (2 * x[n] + x[n-1] - x[n-3] - 2 * x[n-4]);
+	return y[n];
+}
+
+int squaring(int y[]) {
+	int n = 32;
+	y[n] = y[n] * y[n];
+	return y[n];
+}
+
+int movingWindowIntegration(int x[], int y[]) {
+	int n = 32;
+	int N = 30; // ????
+	int buf = 0;
+
+	for (int i = 1; i < N; i++) {
+		buf += x[n-(N-i)];
+	}
+
+
+	y[n] = 1 / N * buf;
+	return y[n];
+}
+
+
+
+
+
+
+
+
+
+
--- Assignment1/main.c
+++ Assignment1/main.c
@@ -1,59 +1,62 @@
 #include <stdio.h>
+#include <math.h>
 #include "sensor.h"
 
 // Filter function declerations
-int lowPass(int x[], int y[]);
-int highPass(int x[], int y[]);
-int derivative(int x[], int y[]);
-int squaring(int y[]);
-int movingWindowIntegration(int x[], int y[]);
+int lowPass();
+int highPass();
+int derivative();
+int squaring();
+int movingWindowIntegration();
+
+// arrays
+int x[13], y[10011], lowpass[33], highpass[5], sq[30];
+int n = 1, deri;
 
 // Main program
 int main(int argc, char *argv[]) {
-	int x[32], y[32];
+	while(n < 15) {
+		x[n % 13] = getNextData();
+		//printf("%15d%15d%15d%15d%15d%15d%15d\n", n, x[n%13], lowPass(), highPass(), derivative(), squaring(), movingWindowIntegration());
 
-	printf("%i\n", getNextData());
-	printf("%i\n", getNextData());
+		printf("%15d%15d%15d%15d\n", n, x[n%13], lowPass(), highPass());
+		n++;
+	}
 
 
+	// End of program
 	return 0;
 }
 
-
 // Filters here
-int lowPass(int x[], int y[]) {
-	int n = 32;
-	y[n] = 2 * y[n-1] - y[n-2] + 1 / 32 * (x[n] - 2 * x[n-6] + x[n-12]);
-	return y[n];
+int lowPass() {
+	lowpass[n%33] = 2 * lowpass[(n-1)%33] - lowpass[(n-2)%33] + (x[n%13] - 2 * x[(n-6)%13] + x[(n-12)%13]) / 32;
+	return lowpass[n%33];
 }
 
-int highPass(int x[], int y[]) {
-	int n = 32;
-	y[n] = y[n-1] - x[n] / 32 + x[n-16] - x[n-17] + x[n-32] / 32;
-	return y[n];
+int highPass() {
+	highpass[n%5] = highpass[(n-1)%5] - lowpass[n%33] / 32.0 + lowpass[(n-16)%33] - lowpass[(n-17)%33] + lowpass[(n-32)%33] / 32.0;
+	return highpass[n%5];
 }
 
-int derivative(int x[], int y[]) {
-	int n = 32;
-	y[n] = 1 / 8 * (2 * x[n] + x[n-1] - x[n-3] - 2 * x[n-4]);
-	return y[n];
+int derivative() {
+	deri = 1.0 / 8.0 * (2 * highpass[n%5] + highpass[(n-1)%5] - highpass[(n-3)%5] - 2 * highpass[(n-4)%5]);
+	return deri;
 }
 
-int squaring(int y[]) {
-	int n = 32;
-	y[n] = y[n] * y[n];
-	return y[n];
+int squaring() {
+	sq[n] *= deri;
+	return sq[n];
 }
 
-int movingWindowIntegration(int x[], int y[]) {
-	int n = 32;
+int movingWindowIntegration() {
+
 	int N = 30; // ????
 	int buf = 0;
 
 	for (int i = 1; i < N; i++) {
-		buf += x[n-(N-i)];
+		buf += sq[(n-(N-i))%30];
 	}
-
 
 	y[n] = 1 / N * buf;
 	return y[n];
