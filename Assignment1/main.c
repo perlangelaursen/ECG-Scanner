2dca40cb9fb3a58546bea09123a28864dcfaecc6
feynman <feynman@localhost.localdomain>
2015-09-11 20:09:33 +0200

    Changed getNextData() to successfully stop at EOF and added a readData() method for cleaner checking

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -2,49 +2,74 @@
 #include <math.h>
 #include "sensor.h"
 
+// Not sure if useful...
+//typedef enum { false = 0, true = !false } bool; /* Taken from stackoverflow
+//				http://stackoverflow.com/questions/1921539/using-boolean-values-in-c
+//				By Michael Potter */
+
+
 #define SIZE 33
 #define N 30
 
 // Filter function declarations
-int lowPass(void);
-int highPass(void);
-int derivative(void);
-int squaring(void);
-int movingWindowIntegration(void);
+int lowPass();
+int highPass();
+int derivative();
+int squaring();
+int movingWindowIntegration();
+
+// Additional function
+int readData();
 int calculateIndex(int n, int i);
 void checkIfNIsOutOfBounds();
 
-int x[SIZE], y[SIZE], lowpassArray[SIZE], highpassArray[SIZE], derivativeArray[SIZE], squaredArray[SIZE];
+int x[10001], y[10001], lowpassArray[10001], highpassArray[10001], derivativeArray[10001], squaredArray[10001];
 int n = 0;
 
 // Main program
 int main(int argc, char *argv[]) {
 	// Does not read the entire file
-	while((x[n] = getNextData()) != EOF) {
+
+	printf("%15s%15s%15s\n", "Number", "Data Read", "Output");
+
+	while(readData()) {
 		lowPass();
 		highPass();
 		derivative();
 		squaring();
 		movingWindowIntegration();
 
-		printf("%15d:%15d\n",x[n], y[n]);
+		printf("%15d%15d%15d\n", n + 1, x[n], squaredArray[n]);
 		n++;
-		checkIfNIsOutOfBounds();
+		//checkIfNIsOutOfBounds();
 	}
 
 	// End of program
 	return 0;
 }
 
+int readData() {
+	// Returns 1 if successful, 0 if EOF is read
+	int dataRead = getNextData();
+
+	if (dataRead == -1000) {
+		return 0; // EOF
+	}
+
+	x[n] = dataRead;
+	return 1;
+}
+
+
 // Filter Implementation
-int lowPass(void) {
+int lowPass() {
 	lowpassArray[n] = (2*lowpassArray[calculateIndex(n, 1)]-
 			lowpassArray[calculateIndex(n, 2)]+(x[n]-
 			2*x[calculateIndex(n, 6)]+x[calculateIndex(n, 12)])/32);
 	return lowpassArray[n];
 }
 
-int highPass(void) {
+int highPass() {
 	highpassArray[n]=highpassArray[calculateIndex(n, 1)]-
 			lowpassArray[n]/32+lowpassArray[calculateIndex(n, 16)]-
 			lowpassArray[calculateIndex(n, 17)]+lowpassArray[calculateIndex(n, 32)]/32;
--- Assignment1/sensor.c
+++ Assignment1/sensor.c
@@ -4,11 +4,23 @@ static const char filename[] = "Files/ECG.txt";
 FILE *file;
 
 int getNextData() {
-	int returnVal;
+	int returnVal; // The value which we return
+
+	// Open the file if it isn't already opened
 	if (file == NULL) {
 		file = fopen(filename, "r");
 	}
 
+	// Read the number from the file
 	fscanf(file, "%i", &returnVal);
+
+	// Checks to see if the file has reached its end
+	// and returns -1000 as our indicator that it ended
+	if (feof(file)) {
+		fclose(file);
+		return -1000;
+	}
+
+	// Return this number
 	return returnVal;
 }
